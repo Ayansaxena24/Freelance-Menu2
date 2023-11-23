288d44c86cece9c30dd813994ae0341b13c58dbc
IAmVengeance <halwapuriyoyoyoyo@gmail.com>
2023-11-23 23:32:18 +0530

    updated logo position

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,7 @@ import Sample2 from "../src/assets/Sample2.jpg";
 import Sample3 from "../src/assets/Sample3.jpg";
 import logo from "../src/assets/logo.gif";
 import logoround from "../src/assets/logoround.gif";
-import animation from "../src/assets/animation.gif";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function App() {
   const [loading, setLoading] = useState(false);
@@ -26,50 +25,46 @@ export default function App() {
   return (
     <div className="overflow-hidden min-w-screen max-w-screen mini-h-screen max-h-screen">
       {loading ? (
-        <div>
-        
-        <img
-          src={logo}
-          className="flex justify-center items-center w-screen h-screen absolute scale-y-150 z-10 pr-4"
-        />
-        <img
-          src={logoround}
-          className="flex justify-center items-center absolute top-1/3 left-20 z-0 scale-125"
-        />
+        <div className="flex justify-center items-center w-screen h-screen">
+          <img src={logo} className="scale-y-150 absolute" />
+          <img
+            src={logoround}
+            className="absolute z-0 scale-125 mb-16"
+          />
         </div>
       ) : (
         <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
         >
-        <Swiper
-          effect={"cube"}
-          grabCursor={true}
-          loop={true}
-          cubeEffect={{
-            shadow: true,
-            slideShadows: true,
-            shadowOffset: 20,
-            shadowScale: 0.94,
-          }}
-          // pagination={true}
-          modules={[EffectCube, Pagination]}
-          className="swiper"
-        >
-          {/* <SwiperSlide><img className='w-screen h-screen' src={home} /> */}
-          {/* <p className='flex justify-center text-md text-white absolute top-20 right-0 left-0 animate-bounce'>Swipe Right ---->></p> */}
-          {/* </SwiperSlide> */}
-          <SwiperSlide>
-            <img className="w-screen h-screen " src={Sample2} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="w-screen h-screen " src={Sample3} />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img className="w-screen h-screen " src={Sample1} />
-          </SwiperSlide>
-        </Swiper>
+          <Swiper
+            effect={"cube"}
+            grabCursor={true}
+            loop={true}
+            cubeEffect={{
+              shadow: true,
+              slideShadows: true,
+              shadowOffset: 20,
+              shadowScale: 0.94,
+            }}
+            // pagination={true}
+            modules={[EffectCube, Pagination]}
+            className="swiper"
+          >
+            {/* <SwiperSlide><img className='w-screen h-screen' src={home} /> */}
+            {/* <p className='flex justify-center text-md text-white absolute top-20 right-0 left-0 animate-bounce'>Swipe Right ---->></p> */}
+            {/* </SwiperSlide> */}
+            <SwiperSlide>
+              <img className="w-screen h-screen " src={Sample2} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="w-screen h-screen " src={Sample3} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="w-screen h-screen " src={Sample1} />
+            </SwiperSlide>
+          </Swiper>
         </motion.div>
       )}
     </div>
